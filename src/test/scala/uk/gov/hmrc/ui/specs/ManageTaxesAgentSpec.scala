75dca9db59c0f683754cfdd0b6a6a1f001891540
payal-bonde <133129614+payal-bonde@users.noreply.github.com>
2026-01-15 10:00:44 +0000

    Start a new return integration tests added for agent and org

--- src/test/scala/uk/gov/hmrc/ui/specs/ManageTaxesAgentSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/ManageTaxesAgentSpec.scala
@@ -38,7 +38,7 @@ import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
 import uk.gov.hmrc.selenium.webdriver.{Browser, ScreenshotOnFailure}
 import uk.gov.hmrc.ui.pages.*
 import uk.gov.hmrc.ui.util.Users.LoginTypes.HASDIRECT
-import uk.gov.hmrc.ui.util.Users.UserTypes.{Agent, Organisation}
+import uk.gov.hmrc.ui.util.Users.UserTypes.Agent
 
 class ManageTaxesAgentSpec
     extends AnyFeatureSpec
@@ -51,7 +51,7 @@ class ManageTaxesAgentSpec
     with ScreenshotOnFailure {
 
   Feature("SDLT Management Journey tests for Agent") {
-    Scenario("Manage Returns Homepage Links") {
+    Scenario("Manage Returns Homepage Returns Links") {
       Given("User enters login using the Authority Wizard page")
       AuthWizard.login(HASDIRECT, Agent, "STN001")
       Then("User should be navigated to the home page")
